2e7e49999430bd5f7be62c82fee886454ba13f49
D.Kiran <dkiran100@gmail.com>
2024-05-17 17:32:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from tensorflow import keras
 from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
